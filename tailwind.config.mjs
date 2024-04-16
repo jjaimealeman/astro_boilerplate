172f6785d08a3d28f97aa17072f85be568374c89
Jaime Aleman <jjaimealeman@gmail.com>
2024-04-16 13:15:33 -0600

    feat: add CSS color variables to tailwind.config.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,19 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				// USING MODERN `RGB`
+				accent: 'rgb(var(--color-accent) / <alpha-value>)',
+				light: 'rgb(var(--color-light) / <alpha-value>)',
+				dark: 'rgb(var(--color-dark) / <alpha-value>)',
+				// USING LEGACY `RGBA`
+				accent: 'rgba(var(--color-accent), <alpha-value>)',
+				light: 'rgba(var(--color-light), <alpha-value>)',
+				dark: 'rgba(var(--color-dark), <alpha-value>)',
+			},
+			//
+		},
 	},
 	plugins: [],
 }
