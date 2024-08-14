70607b0906ebe86a396dbc23c9c62d3399671a51
Jaime Aleman <jjaimealeman@gmail.com>
2024-08-14 13:51:58 -0600

    Redefine tables and add Authors.

--- db/config.ts
+++ db/config.ts
@@ -4,41 +4,60 @@ import { defineDb, defineTable, column } from 'astro:db'
 // https://astro.build/db/config
 
 // FUNCTION-BASED STRUCTURE (DEFINETABLE)
-// Defining the users table
-const Users = defineTable({
-  columns: {
-    id: column.number(),
-    name: column.text(),
-    email: column.text()
-  }
-});
+// Defining the authors table
+const Authors = defineTable({
+	columns: {
+		id: column.number({ primaryKey: true }),
+		name: column.text(),
+		createdAt: column.date({ default: new Date() }),
+		updatedAt: column.date({ default: new Date() })
+	}
+})
 
 // Defining the posts table
 const Posts = defineTable({
-  columns: {
-    id: column.number(),
-    title: column.text(),
-    content: column.text(),
-    author_id: column.number()
-  }
-});
+	columns: {
+		id: column.number({ primaryKey: true }),
+		title: column.text(),
+		content: column.text(),
+		authorId: column.number({ references: () => Authors.columns.id, indexed: true }),
+		createdAt: column.date({ default: new Date() }),
+		updatedAt: column.date({ default: new Date() })
+	}
+})
+
+// Defining the users table
+const Users = defineTable({
+	columns: {
+		id: column.number({ primaryKey: true }),
+		name: column.text(),
+		email: column.text(),
+		createdAt: column.date({ default: new Date() }),
+		updatedAt: column.date({ default: new Date() })
+	}
+})
 
 // Defining the comments table
 const Comments = defineTable({
-  columns: {
-    author: column.text(),
-    body: column.text(),
-    likes: column.number(),
-    flagged: column.boolean(),
-    published: column.date(),
-    metadata: column.json()
-  }
-});
+	columns: {
+		id: column.number({ primaryKey: true }),
+		userId: column.number({ references: () => Users.columns.id, indexed: true }),
+		postId: column.number({ references: () => Posts.columns.id, indexed: true }),
+		body: column.text(),
+		likes: column.number(),
+		flagged: column.boolean(),
+		published: column.date(),
+		metadata: column.json(),
+		createdAt: column.date({ default: new Date() }),
+		updatedAt: column.date({ default: new Date() })
+	}
+})
 
 export default defineDb({
-  tables: {
-    users: Users,
-    posts: Posts,
-    comments: Comments
-  }
-});
+	tables: {
+		authors: Authors,
+		posts: Posts,
+		users: Users,
+		comments: Comments
+	}
+})
