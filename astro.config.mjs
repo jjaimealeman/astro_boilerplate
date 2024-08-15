83ace040b5eb094c6dff22d8363981307daa7ed8
Jaime Aleman <jjaimealeman@gmail.com>
2024-08-15 12:17:01 -0600

    Install package astr-tailwind-config-viewer.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-
 import db from "@astrojs/db";
+
+import tailwindConfigViewer from "astro-tailwind-config-viewer";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), db()]
+  integrations: [tailwind(), icon(), db(), tailwindConfigViewer()]
 });
